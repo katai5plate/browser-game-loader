27749ed19ebc146fd4188cd30e83109ffd329d00
katai5plate <katai5plate.sub@gmail.com>
2021-03-02 00:17:45 +0900

    small refactoring

--- nw/_app/App.module.js
+++ nw/_app/App.module.js
@@ -2,6 +2,8 @@ import { html } from "/_app/utils.module.js";
 import GameTable from "/_app/components/GameTable.module.js";
 
 export default ({}) =>
-  html`<div>
-    <${GameTable} />
-  </div>`;
+  html`
+    <div>
+      <${GameTable} />
+    </div>
+  `;
--- nw/_app/components/GameTable.module.js
+++ nw/_app/components/GameTable.module.js
@@ -1,26 +1,28 @@
 import { html, isDir, GAME_DIR } from "/_app/utils.module.js";
 const fs = require("fs-extra");
 
-export default ({}) => html`<table class="table">
-  <thead>
-    <tr>
-      <th>id</th>
-      <th>Folder</th>
-      <th>Type</th>
-      <th>Title</th>
-    </tr>
-  </thead>
-  <tbody>
-    ${fs
-      .readdirSync(GAME_DIR)
-      .filter((folderName) => isDir(`${GAME_DIR}/${folderName}`))
-      .map(
-        (folderName, index) => html`<tr>
-          <th>${index}</th>
-          <td>${folderName}</td>
-          <td>???</td>
-          <td>${folderName}</td>
-        </tr>`
-      )}
-  </tbody>
-</table>`;
+export default ({}) => html`
+  <table class="table">
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>Folder</th>
+        <th>Type</th>
+        <th>Title</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${fs
+        .readdirSync(GAME_DIR)
+        .filter((folderName) => isDir(`${GAME_DIR}/${folderName}`))
+        .map(
+          (folderName, index) => html`<tr>
+            <th>${index}</th>
+            <td>${folderName}</td>
+            <td>???</td>
+            <td>${folderName}</td>
+          </tr>`
+        )}
+    </tbody>
+  </table>
+`;
