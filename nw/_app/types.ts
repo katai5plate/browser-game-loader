d30843b1827878a010a47b6314f8ddd0c39562d2
katai5plate <katai5plate.sub@gmail.com>
2021-03-04 20:43:41 +0900

    add types

---
+++ nw/_app/types.ts
@@ -0,0 +1,45 @@
+interface GameData {
+  folderName: string;
+  title: string;
+  alias: string;
+  isWEB: boolean;
+  type: string;
+  updatedAt: string;
+  playedAt: string;
+  exec?: {
+    path: string;
+    name: string;
+  };
+  screenSize?: {
+    width: number;
+    height: number;
+  };
+  files: string[];
+  indexHTML: {
+    title?: string;
+    meta?: { name?: string; [attr: string]: string }[];
+    link?: { rel?: string; [attr: string]: string }[];
+    script?: {
+      type?: string;
+      src?: string;
+      codeLength?: number;
+      [attr: string]: string | number;
+    }[];
+  };
+  packageJSON: {
+    name?: string;
+    main?: string;
+    "js-flags"?: string;
+    "chromium-args"?: string;
+    window?: {
+      title?: string;
+      width?: number | string;
+      height?: number | string;
+      icon?: string;
+      [key: string]: any;
+    };
+    [key: string]: any;
+  };
+}
+
+type GetProps<C> = C extends (props: infer R) => any ? R : never;
